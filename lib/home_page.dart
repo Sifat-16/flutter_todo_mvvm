0c5b0fadf79a4a59152b79836df754bff71a15bf
Ashik <ashikmehediabid@gmail.com>
2024-11-28 18:35:11 +0600

    update 2.1

---
+++ lib/home_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _State();
+}
+
+class _State extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+        backgroundColor: Colors.grey,
+      ),
+      body: Column(
+        children: [
+          for (int i = 1; i <= 8; i++)
+            Row(
+              children: [
+                for (int j = 1; j <= 8; j++)
+                  Container(
+                    height: MediaQuery.of(context).size.width / 8,
+                    width: MediaQuery.of(context).size.width / 8,
+                    color: (i & 1 == 1)
+                        ? ((j & 1 == 1) ? Colors.black : Colors.white)
+                        : ((j & 1 == 0) ? Colors.black : Colors.white),
+                  )
+              ],
+            )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_mvvm/home_page.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -12,69 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Page1(),
-      title: 'Flutter Demo',
-      theme: ThemeData(
 
 
-        //gfgfgfg
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-
-      ),
-
-      body: Padding(
-        padding: const EdgeInsets.all(2),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.blueGrey,
-          ),
-          child: Column(
-           children: [
-             for(int i = 1;i<=8;i++)
-               Row(
-                 children: [
-                   for(int j = 1;j<=8;j++)
-                     Container(
-                       height: MediaQuery.of(context).size.width/10,
-                       width: MediaQuery.of(context).size.width/10,
-                         color: (i&1==1)? ((j&1 ==1)? Colors.black:Colors.white)  : ((j&1 ==0)? Colors.black:Colors.white) ,
-                     )
-                 ],
-               )
-           ],
-          ),
-        ),
-      ),
-
-    );
-  }
-}
