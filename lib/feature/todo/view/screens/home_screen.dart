97df3329c33bfcc568f7f45e9a5d7586f1b6e7a0
Ashik <ashikmehediabid@gmail.com>
2024-12-03 10:24:24 +0600

    add in HomeScreen from different screen

--- lib/feature/todo/view/screens/home_screen.dart
+++ lib/feature/todo/view/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_todo_mvvm/feature/todo/view/screens/search_screen.dart';
 import 'package:flutter_todo_mvvm/feature/todo/view_model/home_view_model.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
@@ -11,63 +12,59 @@ class HomeScreen extends ConsumerStatefulWidget {
 }
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
-  TextEditingController todoAddController = TextEditingController();
+ // TextEditingController todoAddController = TextEditingController();
 
   int colorExtractor({required String colorString}) {
     return int.parse(colorString, radix: 16);
   }
 
-/*
-  void showTodoColorDialog({required int index}) async {
-    Color? chosenColor;
-    // raise the [showDialog] widget
-    await showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Pick a color!'),
-          content: SingleChildScrollView(
-            child: ColorPicker(
-              pickerColor: ref.read(homeProvider).pickerColor,
-              onColorChanged: (Color color) {
-                chosenColor = color;
-              },
-            ),
-          ),
-          actions: <Widget>[
-            ElevatedButton(
-              child: const Text(
-                'Cancel',
-                style: TextStyle(color: Colors.red),
-              ),
-              onPressed: () {
-                chosenColor = null;
-                Navigator.pop(context);
-              },
-            ),
-            ElevatedButton(
-              child: const Text('Confirm'),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        );
-      },
-    );
-
-    if (chosenColor != null) {
-      ref
-          .read(homeProvider.notifier)
-          .updateColor(hexColor: chosenColor!.toHexString(), index: index);
-    }
-
-    print("Chosen color ${chosenColor?.toHexString()}");
-  }
-
-
-  */
+  // void showTodoColorDialog({required int index}) async {
+  //   Color? chosenColor;
+  //   // raise the [showDialog] widget
+  //   await showDialog(
+  //     context: context,
+  //     barrierDismissible: false,
+  //     builder: (BuildContext context) {
+  //       return AlertDialog(
+  //         title: const Text('Pick a color!'),
+  //         content: SingleChildScrollView(
+  //           child: ColorPicker(
+  //             pickerColor: ref.read(homeProvider).pickerColor,
+  //             onColorChanged: (Color color) {
+  //               chosenColor = color;
+  //             },
+  //           ),
+  //         ),
+  //         actions: <Widget>[
+  //           ElevatedButton(
+  //             child: const Text(
+  //               'Cancel',
+  //               style: TextStyle(color: Colors.red),
+  //             ),
+  //             onPressed: () {
+  //               chosenColor = null;
+  //               Navigator.pop(context);
+  //             },
+  //           ),
+  //           ElevatedButton(
+  //             child: const Text('Confirm'),
+  //             onPressed: () {
+  //               Navigator.pop(context);
+  //             },
+  //           ),
+  //         ],
+  //       );
+  //     },
+  //   );
+  //
+  //   if (chosenColor != null) {
+  //     ref
+  //         .read(homeProvider.notifier)
+  //         .updateColor(hexColor: chosenColor!.toHexString(), index: index);
+  //   }
+  //
+  //   print("Chosen color ${chosenColor?.toHexString()}");
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -80,28 +77,30 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
         padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 12),
         child: Column(
           children: [
-            TextField(
-              controller: todoAddController,
-              maxLines: 4,
-              minLines: 1,
-              decoration: InputDecoration(
-
-                /// ================================================================================
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    ref.read(homeProvider.notifier).addTodo(title: todoAddController.text.trim());
-                    todoAddController.clear();
-                    //  showTodoColorDialog(
-                    //      index: ref.read(homeProvider).todos.length - 1);
-                  },
-                  icon: Icon(Icons.send),
-                ),
-                /// ===================================================================================
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-            ),
+            // TextField(
+            //   controller: todoAddController,
+            //   maxLines: 4,
+            //   minLines: 1,
+            //   decoration: InputDecoration(
+            //     /// ================================================================================
+            //     suffixIcon: IconButton(
+            //       onPressed: () {
+            //         ref
+            //             .read(homeProvider.notifier)
+            //             .addTodo(title: todoAddController.text.trim());
+            //         todoAddController.clear();
+            //         showTodoColorDialog(
+            //             index: ref.read(homeProvider).todos.length - 1);
+            //       },
+            //       icon: Icon(Icons.send),
+            //     ),
+            //
+            //     /// ===================================================================================
+            //     border: OutlineInputBorder(
+            //       borderRadius: BorderRadius.circular(20),
+            //     ),
+            //   ),
+            // ),
             Expanded(
               child: ListView.builder(
                   itemCount: home.todos.length,
@@ -141,9 +140,18 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
                       ),
                     );
                   }),
-            )
+            ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SearchScreen()),
+          );
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ lib/feature/todo/view/screens/search_screen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_todo_mvvm/feature/todo/view_model/home_view_model.dart';
+
+
+class SearchScreen extends ConsumerStatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  ConsumerState<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends ConsumerState<SearchScreen> {
+
+  TextEditingController todoAddController = TextEditingController();
+
+  void showTodoColorDialog({required int index}) async {
+
+    Color? chosenColor;
+    // raise the [showDialog] widget
+    await showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Pick a color!'),
+          content: SingleChildScrollView(
+            child: ColorPicker(
+              pickerColor: ref.read(homeProvider).pickerColor,
+              onColorChanged: (Color color) {
+                chosenColor = color;
+              },
+            ),
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text(
+                'Cancel',
+                style: TextStyle(color: Colors.red),
+              ),
+              onPressed: () {
+                chosenColor = null;
+                Navigator.pop(context);
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Confirm'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    if (chosenColor != null) {
+      ref
+          .read(homeProvider.notifier)
+          .updateColor(hexColor: chosenColor!.toHexString(), index: index);
+    }
+
+    print("Chosen color ${chosenColor?.toHexString()}");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final home = ref.watch(homeProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Search Page"),
+      ),
+
+      body: Column(
+        children: [
+          TextField(
+            controller: todoAddController,
+            maxLines: 4,
+            minLines: 1,
+            decoration: InputDecoration(
+              /// ================================================================================
+              suffixIcon: IconButton(
+                onPressed: () {
+                  ref
+                      .read(homeProvider.notifier)
+                      .addTodo(title: todoAddController.text.trim());
+                  todoAddController.clear();
+                  showTodoColorDialog(
+                      index: ref.read(homeProvider).todos.length - 1);
+                },
+                icon: Icon(Icons.send),
+              ),
+
+              /// ===================================================================================
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
